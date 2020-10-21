c6cf648936220814742096a539f76abccfd668bf
sepehr-gh <sepehrgh01@gmail.com>
2020-10-21 12:44:15 +0330

    Added NodesJoiningTest

--- src/test/java/com/github/ep2p/kademlia/NodesJoiningTest.java
+++ src/test/java/com/github/ep2p/kademlia/NodesJoiningTest.java
@@ -2,73 +2,71 @@ package com.github.ep2p.kademlia;
 
 import com.github.ep2p.kademlia.connection.EmptyConnectionInfo;
 import com.github.ep2p.kademlia.connection.LocalNodeApi;
-import com.github.ep2p.kademlia.connection.NodeApi;
 import com.github.ep2p.kademlia.exception.BootstrapException;
-import com.github.ep2p.kademlia.node.IncrementalNodeIdFactory;
-import com.github.ep2p.kademlia.node.KademliaNode;
-import com.github.ep2p.kademlia.node.KademliaNodeListener;
-import com.github.ep2p.kademlia.node.NodeIdFactory;
+import com.github.ep2p.kademlia.node.*;
 import com.github.ep2p.kademlia.table.RoutingTableFactory;
 import com.github.ep2p.kademlia.table.SimpleRoutingTableFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NodesJoiningTest {
 
     @Test
-    public void canPeersJoinNetwork() throws BootstrapException {
+    public void canPeersJoinNetwork() throws BootstrapException, InterruptedException {
         LocalNodeApi nodeApi = new LocalNodeApi();
         NodeIdFactory nodeIdFactory = new IncrementalNodeIdFactory();
         RoutingTableFactory<EmptyConnectionInfo, Integer> routingTableFactory = new SimpleRoutingTableFactory();
         Common.IDENTIFIER_SIZE = 4;
         Common.REFERENCED_NODES_UPDATE_PERIOD_SEC = 5;
 
+        Map<Integer, List<Node<EmptyConnectionInfo>>> map = new ConcurrentHashMap<>();
+
         KademliaNodeListener<EmptyConnectionInfo> listener = new KademliaNodeListener<EmptyConnectionInfo>() {
             @Override
             public void onReferencedNodesUpdate(KademliaNode kademliaNode, List referencedNodes) {
-                System.out.println("Referenced nodes of "+ kademliaNode.getId() + " -> "+referencedNodes);
-            }
-
-            @Override
-            public void onStartupComplete(KademliaNode<EmptyConnectionInfo> kademliaNode) {
-                System.out.println("Node "+kademliaNode.getId()+" finished bootstrap");
+                map.put(kademliaNode.getId(), referencedNodes);
             }
         };
 
         KademliaNode<EmptyConnectionInfo> node0 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
         LocalNodeApi.registerNode(node0);
         node0.setKademliaNodeListener(listener);
-        KademliaNode<EmptyConnectionInfo> node1 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
-        LocalNodeApi.registerNode(node1);
-        node1.setKademliaNodeListener(listener);
-        KademliaNode<EmptyConnectionInfo> node2 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
-        LocalNodeApi.registerNode(node2);
-        node2.setKademliaNodeListener(listener);
-        KademliaNode<EmptyConnectionInfo> node3 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
-        LocalNodeApi.registerNode(node3);
-        node3.setKademliaNodeListener(listener);
-        KademliaNode<EmptyConnectionInfo> node4 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
-        LocalNodeApi.registerNode(node4);
-        node4.setKademliaNodeListener(listener);
-        KademliaNode<EmptyConnectionInfo> node5 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
-        LocalNodeApi.registerNode(node5);
-        node5.setKademliaNodeListener(listener);
-
         node0.start();
-        node1.bootstrap(node0);
-        node2.bootstrap(node0);
-        node3.bootstrap(node0);
-        node4.bootstrap(node0);
-        node5.bootstrap(node0);
 
-        node5.getRoutingTable().getBuckets().forEach(bucket -> {
-            bucket.getNodeIds().forEach(System.out::println);
-        });
+        for(int i = 1; i < Math.pow(2, Common.IDENTIFIER_SIZE); i++){
+            KademliaNode<EmptyConnectionInfo> nextNode = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
+            LocalNodeApi.registerNode(nextNode);
+            nextNode.setKademliaNodeListener(listener);
+            nextNode.bootstrap(node0);
+        }
 
 
-        while (true){}
-        //this is not a test yet ... just seeing how it works
+        while (map.size() <= Common.IDENTIFIER_SIZE){
+            //wait
+        }
+        Thread.sleep((long)(1.1D * Common.REFERENCED_NODES_UPDATE_PERIOD_SEC * 1000L));
+
+        Assertions.assertTrue(listContainsAll(map.get(0), 1,2,4,8));
+        Assertions.assertTrue(listContainsAll(map.get(1), 0,3,5,9));
+        Assertions.assertTrue(listContainsAll(map.get(2), 3,0,6,10));
+        Assertions.assertTrue(listContainsAll(map.get(3), 2,1,7,11));
+        Assertions.assertTrue(listContainsAll(map.get(15), 14,13,11,7));
+        Assertions.assertTrue(listContainsAll(map.get(7), 6,5,3,15));
+
+    }
+
+    private boolean listContainsAll(List<Node<EmptyConnectionInfo>> referencedNodes, Integer... nodeIds){
+        List<Integer> nodeIdsToContain = Arrays.asList(nodeIds);
+        for (Node<EmptyConnectionInfo> referencedNode : referencedNodes) {
+            if(!nodeIdsToContain.contains(referencedNode.getId()))
+                return false;
+        }
+        return true;
     }
 
 }
--- src/test/java/example/KademliaNodesToReference.java
+++ src/test/java/example/KademliaNodesToReference.java
@@ -16,7 +16,7 @@ public class KademliaNodesToReference {
         }
 
         // Your node id here. Must be in range of 0 -> (2 power identifierSize)
-        int nodeId = 0;
+        int nodeId = 3;
 
         // Extracting nodes with specified distance
         ArrayList<Integer> validNodes = new ArrayList<>();
