d8206849a6bf2cd8d823a8e22e5fdf640ab06106
sepehr-gh <sepehrgh01@gmail.com>
2020-10-21 12:53:42 +0330

    Added leave-rejoin test

---
+++ src/test/java/com/github/ep2p/kademlia/NodesReJoinTest.java
@@ -0,0 +1,110 @@
+package com.github.ep2p.kademlia;
+
+import com.github.ep2p.kademlia.connection.EmptyConnectionInfo;
+import com.github.ep2p.kademlia.connection.LocalNodeApi;
+import com.github.ep2p.kademlia.exception.BootstrapException;
+import com.github.ep2p.kademlia.exception.ShutdownException;
+import com.github.ep2p.kademlia.node.*;
+import com.github.ep2p.kademlia.table.RoutingTable;
+import com.github.ep2p.kademlia.table.RoutingTableFactory;
+import com.github.ep2p.kademlia.table.SimpleRoutingTableFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class NodesReJoinTest {
+
+    @Test
+    public void canPeersLeaveAndRejoin() throws BootstrapException, InterruptedException, ShutdownException {
+        LocalNodeApi nodeApi = new LocalNodeApi();
+        NodeIdFactory nodeIdFactory = new IncrementalNodeIdFactory();
+        RoutingTableFactory<EmptyConnectionInfo, Integer> routingTableFactory = new SimpleRoutingTableFactory();
+        Common.IDENTIFIER_SIZE = 4;
+        Common.REFERENCED_NODES_UPDATE_PERIOD_SEC = 5;
+
+        Map<Integer, List<Node<EmptyConnectionInfo>>> map = new ConcurrentHashMap<>();
+
+        KademliaNodeListener<EmptyConnectionInfo> listener = new KademliaNodeListener<EmptyConnectionInfo>() {
+            @Override
+            public void onReferencedNodesUpdate(KademliaNode kademliaNode, List referencedNodes) {
+                map.put(kademliaNode.getId(), referencedNodes);
+            }
+        };
+
+        KademliaNode<EmptyConnectionInfo> node0 = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
+        LocalNodeApi.registerNode(node0);
+        node0.setKademliaNodeListener(listener);
+        node0.start();
+
+        KademliaNode<EmptyConnectionInfo> node7 = null;
+
+        for(int i = 1; i < Math.pow(2, Common.IDENTIFIER_SIZE); i++){
+            KademliaNode<EmptyConnectionInfo> aNode = new KademliaNode<>(nodeApi, nodeIdFactory, new EmptyConnectionInfo(), routingTableFactory);
+            LocalNodeApi.registerNode(aNode);
+            aNode.setKademliaNodeListener(listener);
+            aNode.bootstrap(node0);
+            if(i == 7)
+                node7 = aNode;
+        }
+
+
+        while (map.size() <= Common.IDENTIFIER_SIZE){
+            //wait
+        }
+        Thread.sleep((long)(1.1D * Common.REFERENCED_NODES_UPDATE_PERIOD_SEC * 1000L));
+
+        Assertions.assertTrue(listContainsAll(map.get(15), 14,13,11,7));
+        Assertions.assertTrue(listContainsAll(map.get(7), 6,5,3,15));
+        System.out.println("7 and 15 know each other");
+
+        //When node7 leaves network, node 15 should no longer hold reference to it
+        assert node7 != null;
+        node7.stop();
+        Thread.sleep((long)(1.1D * Common.REFERENCED_NODES_UPDATE_PERIOD_SEC * 1000L));
+        Assertions.assertTrue(listDoesntContain(map.get(15), 7));
+        System.out.println("15 doesnt know 7 after it left network");
+
+
+        //When node7 comes back to network, node 15 should be informed and reference to it again
+        //We have to recreate node 7, cause once a node shutsdown it cant start again (since executors shutdown too)
+        KademliaNode<EmptyConnectionInfo> finalNode = node7;
+        node7 = new KademliaNode<>(nodeApi, new NodeIdFactory() {
+            @Override
+            public Integer getNodeId() {
+                return 7;
+            }
+        }, new EmptyConnectionInfo(), new RoutingTableFactory() {
+            @Override
+            public RoutingTable getRoutingTable(Object o) {
+                return finalNode.getRoutingTable();
+            }
+        });
+        node7.start();
+        Thread.sleep((long)(1.1D * Common.REFERENCED_NODES_UPDATE_PERIOD_SEC * 1000L));
+        Assertions.assertTrue(listContainsAll(map.get(15), 14,13,11,7));
+        System.out.println("15 knows about 7 again after it rejoined");
+    }
+
+    private boolean listDoesntContain(List<Node<EmptyConnectionInfo>> referencedNodes, Integer... nodeIds){
+        List<Integer> nodeIdsToContain = Arrays.asList(nodeIds);
+        for (Node<EmptyConnectionInfo> referencedNode : referencedNodes) {
+            if(nodeIdsToContain.contains(referencedNode.getId()))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean listContainsAll(List<Node<EmptyConnectionInfo>> referencedNodes, Integer... nodeIds){
+        List<Integer> nodeIdsToContain = Arrays.asList(nodeIds);
+        for (Node<EmptyConnectionInfo> referencedNode : referencedNodes) {
+            if(!nodeIdsToContain.contains(referencedNode.getId()))
+                return false;
+        }
+        return true;
+    }
+
+}
