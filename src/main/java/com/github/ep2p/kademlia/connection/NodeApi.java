bee3f0cc6b675d7f1c4dc7783bd5a77805aab3ee
sepehr-gh <sepehrgh01@gmail.com>
2020-10-22 19:24:01 +0330

    Storage related api is not mandatory

--- src/main/java/com/github/ep2p/kademlia/connection/NodeApi.java
+++ src/main/java/com/github/ep2p/kademlia/connection/NodeApi.java
@@ -9,8 +9,8 @@ public interface NodeApi<C extends ConnectionInfo> {
     PingAnswer ping(Node<C> caller, Node<C> node);
     void shutdownSignal(Node<C> caller, Node<C> node);
     FindNodeAnswer<C> findNode(Node<C> caller, Node<C> node, Integer destination);
-    <K, V> void storeAsync(Node<C> caller, Node<C> requester,  Node<C> node, K key, V value);
-    <K> void getRequest(Node<C> caller, Node<C> requester, Node<C> node, K key);
-    <K, V> void sendGetResults(Node<C> caller, Node<C> requester, K key, V value);
-    <K> void sendStoreResults(Node<C> caller, Node<C> requester, K key, boolean success);
+    default <K, V> void storeAsync(Node<C> caller, Node<C> requester,  Node<C> node, K key, V value){}
+    default <K> void getRequest(Node<C> caller, Node<C> requester, Node<C> node, K key){}
+    default <K, V> void sendGetResults(Node<C> caller, Node<C> requester, K key, V value){}
+    default <K> void sendStoreResults(Node<C> caller, Node<C> requester, K key, boolean success){}
 }
