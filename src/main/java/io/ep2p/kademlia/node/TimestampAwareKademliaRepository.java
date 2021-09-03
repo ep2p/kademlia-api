2ecee34c611c7734f350b9fed1de9d06f065a32e
sepehr-gh <sepehrgh01@gmail.com>
2021-09-03 11:07:26 +0430

    #10 feat: add TimestampAwareKademliaRepository

---
+++ src/main/java/io/ep2p/kademlia/node/TimestampAwareKademliaRepository.java
@@ -0,0 +1,8 @@
+package io.ep2p.kademlia.node;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public interface TimestampAwareKademliaRepository<K,V> extends KademliaRepository<K, V> {
+    Map<K, V> getDataOlderThan(int amount, TimeUnit unit, int size);
+}
