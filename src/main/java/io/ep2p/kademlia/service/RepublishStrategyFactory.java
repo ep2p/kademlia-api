8ad0db12886b75c8f665c9ef798e22a2943dc772
sepehr-gh <sepehrgh01@gmail.com>
2021-09-04 09:50:17 +0430

    #10 feat: configurable provider in RepublishStrategyFactory

--- src/main/java/io/ep2p/kademlia/service/RepublishStrategyFactory.java
+++ src/main/java/io/ep2p/kademlia/service/RepublishStrategyFactory.java
@@ -3,9 +3,22 @@ package io.ep2p.kademlia.service;
 import io.ep2p.kademlia.connection.ConnectionInfo;
 
 public class RepublishStrategyFactory {
+    public static Provider PROVIDER = new DefaultProvider();
 
     public static <ID extends Number, C extends ConnectionInfo, K, V> RepublishStrategy<ID, C, K, V> getRepublishStrategy(){
-        return new DefaultRepublishStrategy<>();
+        return PROVIDER.provide();
+    }
+
+    public interface Provider {
+        <ID extends Number, C extends ConnectionInfo, K, V> RepublishStrategy<ID, C, K, V> provide();
+    }
+
+    public static class DefaultProvider implements RepublishStrategyFactory.Provider {
+
+        @Override
+        public <ID extends Number, C extends ConnectionInfo, K, V> RepublishStrategy<ID, C, K, V> provide() {
+            return new DefaultRepublishStrategy<>();
+        }
     }
 
 }
